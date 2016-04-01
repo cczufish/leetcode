8a86fd1efc27b1a3d7ed43c4279af9385dd09751
cczuysh <718780261@qq.com>
2016-04-01 12:44:00 +0800

    autoCommit

---
+++ swift/leetcode1137-n-th-tribonacci-number.swift
@@ -0,0 +1,16 @@
+
+class Solution {
+    var array = Array(repeating: 0, count: 38)
+
+    func tribonacci(_ n: Int) -> Int {
+        if n == 0 {return 0}
+        else if n == 1 || n == 2 {return 1}
+        if array[n] > 0 {
+            return array[n]
+        }else {
+            array[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
+            return array[n]
+        }
+    }
+}
+
